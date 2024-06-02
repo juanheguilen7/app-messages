c8ca14adb93a5b2bc0858c3186744844d4534dde
Juan <juan_heguilen@hotmail.com>
2024-06-02 20:04:18 -0300

    github-actions

---
+++ src/app/schedule/script.ts
@@ -0,0 +1,38 @@
+import { db } from '../utils/firebase';
+import { getDocs, collection } from 'firebase/firestore';
+import schedule from 'node-schedule';
+import { DateTime } from 'luxon';
+import { scheduleEmail } from '../utils/programSendEmail';
+
+const scheduleEmailsForNextWeek = async () => {
+    try {
+        const usersSnapshot = await getDocs(collection(db, 'users'));
+
+        usersSnapshot.forEach((userDoc) => {
+            const userData = userDoc.data();
+            const uid = userDoc.id;
+
+            (userData.contacts || []).forEach((contact: { timezone: string; time: string; category: string; email: string }) => {
+                const userTimezone = contact.timezone;
+                const now = DateTime.now().setZone(userTimezone);
+
+                for (let i = 0; i < 7; i++) {
+                    const scheduleTime = now.plus({ days: i }).set({
+                        hour: parseInt(contact.time.split(':')[0]),
+                        minute: parseInt(contact.time.split(':')[1])
+                    });
+
+                    schedule.scheduleJob(scheduleTime.toJSDate(), async function () {
+                        await scheduleEmail(uid, contact);
+                    });
+                }
+            });
+        });
+
+        console.log('Scheduled emails for the next week.');
+    } catch (error) {
+        console.error('Error scheduling emails:', error);
+    }
+};
+
+scheduleEmailsForNextWeek();
